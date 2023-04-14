16c98681c813518969589c28482867148a148c4c
Jonathan Aranguri <arangurim@gmail.com>
2023-04-14 01:04:55 -0300

    feat: Adding Firebase configuration

--- src/landing.js
+++ src/landing.js
@@ -1,1 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, onSnapshot } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebaseConfig from '../src/firebaseConfig'
+
+initializeApp(firebaseConfig);
+
 console.log("Hello world we are online!")
