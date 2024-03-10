6a14eed86da9cf29293bcae2c9897cd455da1df4
Jonathan Aranguri <arangurim@gmail.com>
2024-03-10 05:27:49 -0300

    feat: Adding support for Analytics events

--- src/landing.js
+++ src/landing.js
@@ -1,28 +1,27 @@
-
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, onSnapshot } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics, logEvent } from "firebase/analytics";
+import firebaseConfig from '../src/firebaseConfig';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import firebaseConfig from '../src/firebaseConfig'
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-initializeApp(firebaseConfig);
-
-console.log("Hello world we are online!")
+console.log("Hello world, we are online!");
 
 function startPreloaderAnimation() {
-	const preloaderId = "preloader"
-	const preloaderAnimationClasses = 'jumbotron h-100 d-flex align-items-center justify-content-center bg-black fade-out'
+	const preloaderId = "preloader";
+	const preloaderAnimationClasses = 'jumbotron h-100 d-flex align-items-center justify-content-center bg-black fade-out';
 	setTimeout(() => {
-		document.getElementById(preloaderId).className = preloaderAnimationClasses
+		document.getElementById(preloaderId).className = preloaderAnimationClasses;
 		setTimeout(() => {
-			document.getElementById(preloaderId).remove()
-			document.body.className = ''
-		}, 1300)
-	}, 500)
+			document.getElementById(preloaderId).remove();
+			document.body.className = '';
+		}, 1300);
+	}, 500);
 }
 
-window.onload = startPreloaderAnimation()
+window.onload = startPreloaderAnimation();
+
+window.logAnalyticsEvent = function (event) {
+	logEvent(analytics, event);
+};
